1581ad32a5d66364f3b95e79a0465ecdff1aee3c
Yinhao Wang <njwangyh@gmail.com>
2017-08-25 23:08:32 +0800

    ‘add transforming url function

--- crawlBaiduImg.py
+++ crawlBaiduImg.py
@@ -3,14 +3,26 @@ import  re
 import traceback
 def getHTML(url):
     try:
-        head='Mozilla/5.0 '
+        head='Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36 '
         r=requests.get(url,headers={'User-Agent':head})
         r.raise_for_status()
         r.encoding=r.apparent_encoding
         return r.text
     except:
         traceback.print_exc()
-
+def transURL(originURL):
+    transDict={'w':'a','k':'b','v':'c','1':'d','j':'e','u':'f','2':'g','i':'h','t':'i','3':'j','h':'k','s':'l','4':'m',
+               'g':'n','5':'o','r':'p','q':'q','6':'r','f':'s','p':'t','7':'u','e':'v','o':'w','8':'1','d':'2','n':'3',
+               '9':'4','c':'5','m':'6','0':'7','b':'8','l':'9','a':'0',':':':','.':'.','/':'/','-':'-'}
+    newURL=''
+    originURL=originURL.replace('_z2C$q',':')
+    originURL=originURL.replace('_z&e3B','.')
+    originURL=originURL.replace('AzdH3F',r'/')
+    originURL = originURL.lower()
+    print(originURL)
+    for i in originURL:
+        newURL=newURL+transDict[i]
+    return newURL
 
 def parseDownloadLink(dList,html):
 
@@ -22,6 +34,8 @@ def parseDownloadLink(dList,html):
         except:
             traceback.print_exc()
             continue
+def parseJsonDownloadLink():
+    pass
 
 def saveImg(dList,fpath):
     for i in range(len(dList)):
--- test.py
+++ test.py
@@ -1,8 +1,16 @@
-import requests
-
+test='ippr_z2C$qAzdH3FAzdH3Ft-0_z&e3Bevt42_z&e3Bv54AzdH3Fp6t4AzdH3F1uk91bbjuujkbwdvlldb1klnd09bnvlbdbbnmddAzdH3Fp6t4_z&e3B3r2'
+def transURL(originURL):
+    transDict={'w':'a','k':'b','v':'c','1':'d','j':'e','u':'f','2':'g','i':'h','t':'i','3':'j','h':'k','s':'l','4':'m',
+               'g':'n','5':'o','r':'p','q':'q','6':'r','f':'s','p':'t','7':'u','e':'v','o':'w','8':'1','d':'2','n':'3',
+               '9':'4','c':'5','m':'6','0':'7','b':'8','l':'9','a':'0',':':':','.':'.','/':'/','-':'-'}
+    newURL=''
+    originURL=originURL.replace('_z2C$q',':')
+    originURL=originURL.replace('_z&e3B','.')
+    originURL=originURL.replace('AzdH3F',r'/')
+    originURL = originURL.lower()
+    print(originURL)
+    for i in originURL:
+        newURL=newURL+transDict[i]
+    return newURL
 if __name__ == '__main__':
-    r=requests.get('http:\/\/pic.chinadaily.com.cn\/img\/attachement\/jpg\/site1\/20151010\/448a5bd66b2f1782c88d20.jpg'.replace('\\',''))
-    r.encoding=r.apparent_encoding
-    with open('x.jpg','wb') as f:
-        f.write(r.content)
-        f.close()
+    print(transURL(test))
