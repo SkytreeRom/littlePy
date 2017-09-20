00a645dea1ce9e2e4084edee10475a950521dc00
Yinhao Wang <njwangyh@gmail.com>
2017-09-20 15:54:53 +0800

    implement basic function

---
+++ jsonser.py
@@ -0,0 +1,24 @@
+import simplejson
+from flask import Response
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def get_json2():
+    data = {
+        "abandon": [{"time": [1, 2, 3, 4],
+                     "time2": [1, 2, 3, 4]}],
+        "abandon2": [{"time": [1, 2, 3, 4],
+                      "time2": [1, 2, 3, 4]}]
+    }
+    format = simplejson.dumps(data)
+    return Response(
+        response=format,
+        mimetype="application/json",
+        status=200
+    )
+
+
+app.run()
